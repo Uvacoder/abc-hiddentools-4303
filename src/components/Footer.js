eeb5833a29f9a9216f622aa279f599e6852945bd
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-26 03:51:06 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
             <div className="footer">
 	            hidden tools by - <a aria-label="uvacoder Twitter" href="https://twitter.com/AlecCam43544378" target="_blank" rel="noreferrer">@AlecCam43544378</a> 🤞🏻
                 <br/>
-                follow us on <a aria-label="Hidden Tools' Twitter" href="https://twitter.com/hiddentools_" target="_blank" rel="noreferrer">twitter</a>, open-source on<a aria-label="Hidden Tools' Github" href="https://github.com/uvacoder/abc-hiddentools" target="_blank" rel="noreferrer"> github</a>.
+                follow us on <a aria-label="Hidden Tools' Twitter" href="https://twitter.com/hiddentools_" target="_blank" rel="noreferrer">tweetter</a>, open-source on<a aria-label="Hidden Tools' Github" href="https://github.com/uvacoder/abc-hiddentools" target="_blank" rel="noreferrer"> github</a>.
             </div>
         );
     }
